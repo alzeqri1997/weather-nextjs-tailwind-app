d6b487e3b6fddba1c2af208d0b642e7a4fb0da29
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2021-12-10 22:37:48 +0300

    include MainCard & ContentBox Components

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,42 @@
-import Head from 'next/head';
+import { useState } from 'react';
+import useSWR from 'swr';
+import MainCard from '../components/MainCard';
+import ContentBox from '../components/ContentBox';
 
-export default function Home({ data }) {
-  return (
-    <div>
-      <Head>
-        <title>Weather App </title>
-        <meta
-          name="description"
-          content="Weather App built on top of NextJS and TailwindCSS"
+const fetcher = (URL) => fetch(URL).then((res) => res.json());
+
+export default function Home() {
+  const [searchQuery, setSearchQuery] = useState('yemen');
+  const [systemUsed, setSystemUsed] = useState('metric');
+  const { data: weatherData, error } = useSWR(
+    `https://api.openweathermap.org/data/2.5/weather?q=${searchQuery}&appid=${process.env.NEXT_PUBLIC_OPENWEATHER_API_KEY}`,
+    fetcher
+  );
+
+  return weatherData && !weatherData.message && !error ? (
+    <div
+      style={{ minHeight: '500px' }}
+      className="grid place-items-center h-screen "
+    >
+      <div className="rounded-lg overflow-hidden min-w-min shadow-lg text-center bg-gray-100 flex ">
+        <MainCard
+          city={weatherData.name}
+          country={weatherData.country}
+          iconName={weatherData.weather[0].icon}
+          systemUsed={systemUsed}
+          weatherData={weatherData}
+          description={weatherData.weather[0].description}
         />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1>this is the main app</h1>
-      </main>
+        <ContentBox
+          weatherData={weatherData}
+          setSearchQuery={setSearchQuery}
+          systemUsed={systemUsed}
+        />
+      </div>
     </div>
+  ) : weatherData && weatherData.message ? (
+    <h1>{weatherData.message}</h1>
+  ) : (
+    <h1>...loading</h1>
   );
 }
