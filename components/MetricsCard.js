3407c67281fe2ba6929222cad71097a12e523406
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2021-12-10 22:30:32 +0300

    add MetricsCard Component

---
+++ components/MetricsCard.js
@@ -0,0 +1,16 @@
+import Image from 'next/image';
+
+export default function MetricsCard({ title, iconSrc, metric, unit }) {
+  console.log(iconSrc);
+  return (
+    <div className="rounded-lg flex flex-auto p-4 bg-gray-100">
+      <Image alt="weatherIcon" width="70px" height="70px" src={iconSrc} />
+
+      <div className="text-right flex-1">
+        <p className="text-gray-400">{title}</p>
+        <p className="text-2xl">{metric}</p>
+        <p className=" text-xs">{unit}</p>
+      </div>
+    </div>
+  );
+}
